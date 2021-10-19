59b3d1302d02e9d81f63d3c61a7e1ab788437f80
badger <badger@badger.local>
2021-10-19 18:34:47 +0300

    14. Resolving wrong animated scale

--- showcase/src/patterns/01.js
+++ showcase/src/patterns/01.js
@@ -24,6 +24,9 @@ const withClapAnimation = WrappedComponent => {
         easing: mojs.easing.ease.out
       })
 
+      const clap = document.getElementById('clap');
+      clap.style.transform = 'scale(1,1)'
+
       const newAnimationTimeline = this.animationTimeline.add([
         scaleButton 
       ])
